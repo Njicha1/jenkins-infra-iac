c4a754b355b3572347fa39f2f8692502b3f4bfc7
etechConsultingDevops <99133447+etechConsultingDevops@users.noreply.github.com>
2024-03-30 16:05:23 -0400

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,4 +1,4 @@
-#!/bin/bash #specifies the interpreter
+#!/bin/bash 
 sudo yum update -y  # updates the package list and upgrades installed packages on the system
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo  #downloads the Jenkins repository configuration file and saves it to /etc/yum.repos.d/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key  #imports the GPG key for the Jenkins repository. This key is used to verify the authenticity of the Jenkins packages
